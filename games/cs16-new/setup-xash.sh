25cbec92c8b8c1e4686352dd16fa51feff31a479
Wild <samwild9999@gmail.com>
2026-06-14 15:24:52 +0100

    Delete games/cs16-new directory

--- games/cs16-new/setup-xash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# webxash-fwgs
-cp -r node_modules/xash3d-fwgs public/
-# hl
-cp -r node_modules/hlsdk-portable public/
-# cs
-cp -r node_modules/cs16-client public/
-
--- games/cs16-new/src/services/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SaveManager } from './save-manager.ts'
-export { default as XashLoader } from './xash-loader.ts'
